714045bd7ee7b998e107574ea10c0b931154b01b
LEESANGHEON1107 <dltkdgjs1@hanmail.net>
2023-04-10 17:16:35 +0900

    First Commit

---
+++ pages/posts/first-page.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+export default function FirstPage() {
+    return (
+        <div>
+            <h2>Hello.</h2>
+            <h5>My hamster is very cute</h5>
+            <p>내가 제일 좋아하는 영화는 배틀쉽이다.</p>
+            <ul>
+                <li>처음 해보는거라서 익숙치 않다</li>
+                <li>처음 해보는거라서 익숙치 않다</li>
+                <li>처음 해보는거라서 익숙치 않다</li>
+            </ul>
+        </div>
+
+
+    )
+}
